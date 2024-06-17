a06ad1004db8cc942a5a597b61bd6fb116d04a39
diegocacau <dc_cacau@hotmail.com>
2024-06-17 16:03:39 -0300

    atualizando csv com numero de mortos

--- fetch_data.py
+++ fetch_data.py
@@ -2,21 +2,19 @@ from datetime import datetime
 from covid19dh import covid19
 
 
-x, src = covid19("BRA", level = 1, verbose = False, end = "2022-12-30")
+x, src = covid19("BRA", level = 3, verbose = False)
+
+x.to_csv('total_data.csv')
 
 # print(x['date'],x['deaths'])
 
+# for i in x.head():
+# 	print(i)
 
-# for i in x:
-# 	total = i['confirmed'] - i['deaths'] - i['recovered']
-# 	print(total)
 
-# x['new_deaths'] = (x.groupby(['date'])['deaths']
-#                           .diff()
-#                           .fillna(x['deaths'])						
-#                           .astype(int))
+# x['new_deaths'] = (x['deaths']).diff()._get_numeric_data()
 
-x['new_deaths'] = (x['deaths']).diff()._get_numeric_data()
+# x['new_cases'] = x['confirmed'] - x['deaths'] - x['recovered']
+# x.to_csv('output.csv', columns=['date','deaths', 'confirmed', 'new_cases', 'new_deaths'])
 
-x['new_cases'] = x['confirmed'] - x['deaths'] - x['recovered']
-x.to_csv('output.csv', columns=['date','deaths', 'confirmed', 'new_cases', 'new_deaths'])
+# x.to_csv('output_new_deaths.csv', columns=['date', 'new_deaths'])
